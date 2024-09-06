575b72ef5ee3ce51f482b02592db66c3344d416e
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2024-09-06 11:11:18 +0900

    古いプロジェクトの消去

--- YasyaProjectVer1_2/Source/YasyaProjectVer1/YasyaProjectVer1.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "YasyaProjectVer1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, YasyaProjectVer1, "YasyaProjectVer1" );
- 
